680b3b74d09b0629e0887aeb06080a66ed039b87
Manpal-Singh <manpalsingh25061996@gmail.com>
2025-09-09 23:43:34 +0530

    pushed initial setup project

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+import AppLayout from './Components/Body/AppLayout'
+
+function App() {
+
+  return (
+    <>
+      <AppLayout />
+    </>
+  )
+}
+
+export default App
---
+++ src/main.jsx
@@ -0,0 +1,35 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+  Link,
+} from "react-router-dom";
+import "./index.css";
+import App from "./App.jsx";
+import About from "./Pages/About.jsx";
+import Contact from "./Pages/Contact.jsx";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "about",
+        element: <About />,
+      },
+      {
+        path: "contact",
+        element: <Contact />,
+      },
+    ],
+  },
+]);
+
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <RouterProvider router={router} />
+  </StrictMode>
+);
